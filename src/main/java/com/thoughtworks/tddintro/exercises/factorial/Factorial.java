51bcd4248cdf019a1a8230cefcc68f40cf9ecd97
Andrea Allen <andrea2allen@gmail.com>
2018-01-23 16:17:09 -0500

    First and second tests pass.

--- src/main/java/com/thoughtworks/tddintro/exercises/factorial/Factorial.java
+++ src/main/java/com/thoughtworks/tddintro/exercises/factorial/Factorial.java
@@ -3,7 +3,7 @@ package com.thoughtworks.tddintro.exercises.factorial;
 public class Factorial {
     public Integer compute(int i) {
 
-        if(i==1){
+        if(i>0){
             return i;
         }
 
