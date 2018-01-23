7f0430f5b567a03859ebc995c7f5058920fc0886
Andrea Allen <andrea2allen@gmail.com>
2018-01-23 17:39:36 -0500

    First test implemented, Account class modified, passes first test.

--- src/main/java/com/thoughtworks/tddintro/exercises/accountbalance/Account.java
+++ src/main/java/com/thoughtworks/tddintro/exercises/accountbalance/Account.java
@@ -1,4 +1,29 @@
 package com.thoughtworks.tddintro.exercises.accountbalance;
 
 public class Account {
+    private int balance;
+
+    public Account(){
+
+    }
+
+    public Account(int i){
+        this.balance=i;
+    }
+
+    public void deposit(int i){
+        setBalance(this.getBalance()+i);
+    }
+
+    public void withdraw(int i){
+
+    }
+
+    public int getBalance(){
+        return this.balance;
+    }
+
+    public void setBalance(int i){
+        this.balance=i;
+    }
 }
